1f676af3e25432e71a3089a6458d731ae09ca025
ahyiru <ah.yiru@gmail.com>
2017-05-04 15:57:17 +0800

    init package.json

--- app/components/maindemo.js
+++ app/components/maindemo.js
@@ -10,6 +10,12 @@ let main=[{
   values:'-',
   default:'-'
 },{
+  key:'hidePageHead',
+  expr:'是否隐藏头部内容',
+  type:'boolean',
+  values:'true/false',
+  default:'false'
+},{
   key:'hidePagetitle',
   expr:'是否显示面包屑上面标题',
   type:'boolean',
@@ -17,14 +23,29 @@ let main=[{
   default:'false'
 }];
 
+const t=`<Main breadcrumb={current} hidePageHead={true} /*hidePagetitle={false}*/>
+    {this.props.children}
+  </Main>
+`;
+
 export default class MainDemo extends React.Component<any,any> {
 
   render() {
     return (
       <Items>
         <Item>
-          <h2>barnd配置</h2>
-          <Table thead={thead} tbody={main} noBorder={true} />
+          <div className="textarea">
+            <h2>Phoenix UI</h2>
+            <p>Phoenix UI是一套用于快速开发web应用的UI框架。</p>
+            <p>Phoenix UI使用Typescript编写，提供一些简单UI组件及函数库。拓展性强，可配合其它一些组件或插件进行开发，如ant design、jquery等。</p>
+            <h2>路由配置</h2>
+            <p>本框架采用html5的popstate来控制路由切换，配置方便，便于同构。</p>
+            <Code title="demo" code={t} />
+          </div>
+          <Row>
+            <h2>barnd配置</h2>
+            <Table thead={thead} tbody={main} noBorder={true} />
+          </Row>
         </Item>
       </Items>
     )
